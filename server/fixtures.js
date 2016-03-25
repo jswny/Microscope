1b84e74436c61ed75f2efb14fa225f8b39f672fc
Jeteroll <jeteroll@igetthat.money>
2016-03-25 15:10:11 -0400

    Added basic upvoting algorithm

--- server/fixtures.js
+++ server/fixtures.js
@@ -17,14 +17,16 @@ if (Posts.find().count() === 0) {
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0
   });
 
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
     author: tom.profile.name,
-    submitted: new Date(now - 7 * 3600 * 1000),
+    submitted: new Date(now - 5 * 3600 * 1000),
     body: 'Interesting project Sacha, can I get involved?'
   });
 
@@ -32,7 +34,7 @@ if (Posts.find().count() === 0) {
     postId: telescopeId,
     userId: sacha._id,
     author: sacha.profile.name,
-    submitted: new Date(now - 7 * 3600 * 1000),
+    submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
 
@@ -41,17 +43,21 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
-    submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 0
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
 
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
     author: tom.profile.name,
-    url: 'http://meteor.com',
-    submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 0
+    url: 'http://themeteorbook.com',
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
 
   for (var i = 0; i < 10; i++) {
@@ -60,8 +66,10 @@ if (Posts.find().count() === 0) {
       author: sacha.profile.name,
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
-      submitted: new Date(now - i * 3600 * 1000),
-      commentsCount: 0
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
     });
   }
 }
